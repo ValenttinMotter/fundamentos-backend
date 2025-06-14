66c7b7be7536f5ff4aabc6e729a3613045d6dfd6
Valenttin Motter <valenttin@myscrumhalf.com>
2025-06-14 15:08:02 -0300

    feat: finished users module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,15 +1,9 @@
 import { Module } from "@nestjs/common";
-import { PrismaService } from "./prisma.service";
-
-import { CreateModelService } from "./models/create-model.service";
-import { DeleteModelService } from "./models/delete-model.service";
-import { EditModelService } from "./models/edit-model.service";
-import { FetchRecentModelsService } from "./models/fetch-recent-models.service";
-import { GetModelByIdService } from "./models/get-model-by-id.service";
 import { ProductsModule } from "./products/products.module";
 import { ModelsModule } from "./models/models.module";
+import { UsersModule } from "./users/users.module";
 
 @Module({
-  imports: [ProductsModule, ModelsModule],
+  imports: [ProductsModule, ModelsModule, UsersModule],
 })
 export class AppModule {}
