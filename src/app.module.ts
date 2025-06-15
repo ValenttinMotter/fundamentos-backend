1e745b0ce96c6230999f691da9dba7e78bc561e1
Valenttin Motter <valenttin@myscrumhalf.com>
2025-06-14 21:55:35 -0300

    feat: finished profiles repository

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,8 +2,9 @@ import { Module } from "@nestjs/common";
 import { ProductsModule } from "./products/products.module";
 import { ModelsModule } from "./models/models.module";
 import { UsersModule } from "./users/users.module";
+import { ProfilesModule } from "./profiles/profiles.module";
 
 @Module({
-  imports: [ProductsModule, ModelsModule, UsersModule],
+  imports: [ProductsModule, ModelsModule, UsersModule, ProfilesModule],
 })
 export class AppModule {}
